054b88e6696139d72e54d52398769d538a994ac7
Michael DeAngelo <deangelomp@gmail.com>
2018-06-18 15:44:50 -0500

    added dao file for the severity class

---
+++ src/main/java/me/bedaring/imsproject/models/data/SeverityDao.java
@@ -0,0 +1,12 @@
+package me.bedaring.imsproject.models.data;
+
+import me.bedaring.imsproject.models.Severity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface SeverityDao extends CrudRepository<Severity, Integer> {
+}
